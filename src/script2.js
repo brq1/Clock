45576af05fb68f06f29037f1d883931fe7931bb6
brq1 <brq2@o2.pl>
2019-04-30 13:59:15 +0200

    In progress... 1) edit HTML - remove inline code, <br> tags, add div structure; 2) edit JS - separation functions, addEventListener
    to fixed: timer

--- src/script2.js
+++ src/script2.js
@@ -1,26 +1,91 @@
+var ms = new Date();
+var s, m, h;
+var timeZone = 2;
+var AmPm = 0;
 
-function clock(){;
-  var date = new Date();
-  var h = date.getHours();
-  var m = date.getMinutes();
-  var s = date.getSeconds();
-  var ms = (date.getMilliseconds()/10).toFixed(0);
-  var AmPm = document.getElementById("AmPm");
-//AM / PM mode on
-  if (AmPm.checked == true) {
-    h = (h>12) ? h - 12 : h;
-  }
-//display method  
-  h = (h<10) ? "0"+h : h;
-  m = (document.getElementById("min").checked === true) ? ((m<10) ? "0"+m : m) : "--";
-  s = (document.getElementById("sec").checked === true) ? ((s<10) ? "0"+s : s) : "--";
-  ms = (document.getElementById("msc").checked === true) ? ((ms<10) ? "0"+ms : ms) : "--";
-  
-  var timer = h +":"+ m +":"+ s + ":" + ms;
 
-  document.getElementById("display").innerHTML = timer;
-
-  setInterval(clock,1000);
-
+var div = {
+  ms : document.getElementById("msec"),
+  s : document.getElementById("sec"),
+  m : document.getElementById("min"),
+  h : document.getElementById("hour")
 }
 
+var box = {
+  ms : document.getElementById("mscBox"),
+  s : document.getElementById("secBox"),
+  m : document.getElementById("minBox")
+}
+
+function timer (){
+  ms++;
+}
+
+function getTime(){
+  setTimeout(timer,1);
+  s = Math.floor(ms/1000);
+  m = Math.floor(s/60);
+  h = Math.floor(m/60) + timeZone - (AmPm*12);
+
+  ms %= 1000;
+  s %= 60;
+  m %= 60;
+  h %= 24;
+
+  h = (h<10) ? "0"+h : h;
+  m = (m<10) ? "0"+m : m;
+  s = (s<10) ? "0"+s : s;
+  ms = (ms<10) ? "0"+ms : ms;
+}
+
+window.onload = function time(){
+      getTime();
+      div.ms.innerText = ms;
+      div.s.innerText = s;
+      div.m.innerText = m;
+      div.h.innerText = h;
+      //setInterval(getTime,1);
+}
+
+box.ms.addEventListener("change", function hideMs(){
+  if (this.checked) {
+    div.ms.style.visibility = "visible";
+  } else {
+    div.ms.style.visibility = "hidden";
+  }
+});
+
+box.s.addEventListener("change", function hideS(){
+  if (this.checked) {
+    div.s.style.visibility = "visible";
+  } else {
+    div.s.style.visibility = "hidden";
+  }
+});
+
+box.m.addEventListener("change", function hideM(){
+  if (this.checked) {
+    div.m.style.visibility = "visible";
+  } else {
+    div.m.style.visibility = "hidden";
+  }
+});
+
+var radio = document.getElementById("radioAmPm");
+
+radio.addEventListener("change", function radio(){
+  if (this.checked) {
+    AmPm = 1
+  } else {
+    AmPm = 0;
+  }
+});
+
+ /* radio[i].addEventListener("click", function mode(){
+    for (var i=0; i<2; i++) {
+      if (radio[i].checked) {
+        radio[i].value
+      }
+    }
+  }
+  )*/
