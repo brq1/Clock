232fd6071e902cb94accbb3ebb109328f74e2579
brq1 <brq2@o2.pl>
2019-05-02 19:04:21 +0200

    add mode AM/PM and 24H with addEvenListener

--- src/script2.js
+++ src/script2.js
@@ -4,7 +4,7 @@ var s = currentTime.getSeconds;
 var m = currentTime.getMinutes;
 var h = currentTime.getHours;
 var timeZone = 2;
-var pm = true;
+var pm = false;
 
 
 var div = {
@@ -21,22 +21,11 @@ var box = {
   m : document.getElementById("minBox")
 }
 
-
 window.onload = function getTime(){
-  ms = Math.floor(currentTime/10);
-  s = Math.floor(currentTime/1000);
-  m = Math.floor(currentTime/60000);
-  h = Math.floor(currentTime/3600000) + timeZone;
-
-  ms %= 100;
-  s %= 60;
-  m %= 60;
-  h %= 24;
-
-  h = (h<10) ? "0"+h : h;
-  m = (m<10) ? "0"+m : m;
-  s = (s<10) ? "0"+s : s;
-  ms = (ms<10) ? "0"+ms : ms;
+  ms = (Math.floor(currentTime/10))%100;
+  s = (Math.floor(currentTime/1000))%60;
+  m = (Math.floor(currentTime/60000))%60;
+  h = (Math.floor(currentTime/3600000))%24 + timeZone;
 
   dispay();
   currentTime = (currentTime*1+10);
@@ -44,28 +33,19 @@ window.onload = function getTime(){
 }
 
 function dispay(){
+
+  h = (h<10) ? "0"+h : h;
+  m = (m<10) ? "0"+m : m;
+  s = (s<10) ? "0"+s : s;
+  ms = (ms<10) ? "0"+ms : ms;
+
     div.ms.innerText = ms;
     div.s.innerText = s;
     div.m.innerText = m;
     div.h.innerText = h - (pm*12);
-
 }
 
 
-function pm (){  
-  
-  var testH = 24 - currentTime.getHours();
-  var revision = 86400000-currentTime; // time to end day
-  
-  if (testH >= 12){
-    div.Am.innerHTML = "PM";
-    pm = true;
-  } else{
-    div.Am.innerHTML = "AM";
-    pm = false;
-  }
-  setTimeout(pm,revision);
-}
 
 box.ms.addEventListener("change", function hideMs(){
   if (this.checked) {
@@ -92,4 +72,22 @@ box.m.addEventListener("change", function hideM(){
 });
 
 var radio = document.getElementsByName("radio");
+var testH = 24 - currentTime.getHours();
+var revision = 86400000-currentTime; // time to end day
 
+for (var i = 0;i<2;i++){
+this.addEventListener("change", function ampm(){
+  if (radio[0].checked){
+    if (testH < 12){
+      div.Am.innerHTML = "PM";
+      pm = true;
+    } else{
+      div.Am.innerHTML = "AM";
+      pm = false;
+    }
+    setTimeout(pm,revision);
+  } else {
+    div.Am.innerHTML = "";
+    pm = false
+  }
+});}
