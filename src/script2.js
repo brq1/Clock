e138a93462ab9179994fc74b3aea6aec9151b120
brq1 <brq2@o2.pl>
2019-05-23 22:20:54 +0200

    fix functions: displayTime, getTime, clock

--- src/script2.js
+++ src/script2.js
@@ -1,11 +1,6 @@
 var currentTime = new Date();
-var ms = currentTime.getMilliseconds;
-var s = currentTime.getSeconds;
-var m = currentTime.getMinutes;
-var h = currentTime.getHours;
 var timeZone = 2;
 var pm = false;
-
 
 var div = {
   ms : document.getElementById("msec"),
@@ -18,32 +13,45 @@ var div = {
 var box = {
   ms : document.getElementById("mscBox"),
   s : document.getElementById("secBox"),
-  m : document.getElementById("minBox")
+  m : document.getElementById("minBox"),
+
+}
+var radio = {
+  mode : document.getElementsByClassName("radio")
 }
 
 window.onload = function clock(){
   getTime()
   displayTime()
-  currentTime = currentTime*1+10;
   setTimeout(clock,10);
 }
 
 function getTime(){
-  ms = (Math.floor(currentTime/10))%100; // getMilliseconds
-  s = (Math.floor(currentTime/1000))%60; // getSeconds
-  m = (Math.floor(currentTime/60000))%60; // getMinutes
-  h = (Math.floor(currentTime/3600000))%24 + timeZone; // getHours
+  currentTime = new Date;
+
+  ms = currentTime.getMilliseconds();
+  s = currentTime.getSeconds();
+  m = currentTime.getMinutes();
+  h = currentTime.getHours();
+}
+
+function trimHour (hour) {
+  return pm ? hour%12 : hour;
 }
 
 function displayFix(x){ 
   return (x<10) ? "0"+x : x;
 }
  
+function trimMilliseconds(v){
+  return (v>100) ? Math.floor(v/10) : v;
+}
+
 function displayTime(){
-  div.ms.innerText = displayFix(ms);
+  div.ms.innerText = trimMilliseconds(displayFix(ms));
   div.s.innerText = displayFix(s);
   div.m.innerText = displayFix(m);
-  div.h.innerText = displayFix(h - (pm*12));
+  div.h.innerText = displayFix(trimHour(h));
 }
 
 function hideTime (box,target){
@@ -55,27 +63,20 @@ function hideTime (box,target){
     }
 }
 
+function changeMode() {
+  if (pm == true) {
+      if (h<12) {
+        div.Am.innerHTML = "AM";
+      } else {
+        div.Am.innerHTML = "PM";
+      }
+  } else {
+    div.Am.innerHTML = "";
+  }
+  pm = !pm;
+}
+
 box.ms.addEventListener("change", () => hideTime(box.ms, div.ms));
 box.s.addEventListener("change", () => hideTime(box.s,div.s));
 box.m.addEventListener("change", () => hideTime(box.m,div.m));
-
-
-var radio = document.getElementsByName("radio");
-const halfDay = 43200000; // 12:00 it's hour to change AM to PM
-var revision = halfDay - (currentTime*1+timeZone*3600000)%halfDay; // time to change AM to PM
-
-for (var i = 0;i<2;i++){
-this.addEventListener("change", function ampm(){
-  if (radio[0].checked){
-    if (h > 12){
-      div.Am.innerHTML = "PM";
-      pm = true;
-    } else{
-      div.Am.innerHTML = "AM";
-      pm = false;
-    }
-    setTimeout(ampm,revision);
-  } else {
-    div.Am.innerHTML = "";
-    pm = false
-  }})}
+radio.mode.addEventListener("click", changeMode);
